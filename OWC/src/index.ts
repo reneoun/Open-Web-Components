7e50824cd1b9887fe929a39956a446b4ddb1384a
reneoun <43538982+reneoun@users.noreply.github.com>
2026-03-25 23:33:55 +0100

    feat: wire o-search exports and add demo

--- OWC/src/index.ts
+++ OWC/src/index.ts
@@ -2,11 +2,13 @@ import './core'
 import './table'
 import { toast } from './toast'
 import './toggle'
+import './search'
 
 export * from './core'
 export * from './toast'
 export * from './table'
 export * from './toggle'
+export * from './search'
 
 // Expose toast() as a global for plain <script> usage
 if (typeof window !== 'undefined') {
