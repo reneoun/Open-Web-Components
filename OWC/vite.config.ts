6cebd2ec39b6d5eec783b075a96fddec8f66eac2
reneoun <43538982+reneoun@users.noreply.github.com>
2026-04-07 22:48:12 +0200

    feat: add ESM build + @owc/components npm config

--- OWC/vite.config.ts
+++ OWC/vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      formats: ['es'],
+      fileName: 'owc',
+    },
+  },
   test: {
     environment: 'happy-dom',
   },
